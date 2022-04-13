8ce0c7c88b9bad0b00073ad9d5215ac7a8705954
Roman <siniavsky.rom@mail.ru>
2022-04-13 19:58:35 +0300

    added seconds, added ag color an opacity

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ function calculateTime() {
     let dayNumb = date.getDay();
     let hour = date.getHours();
     let minute = date.getMinutes();
+    let seconds = date.getSeconds();
     let ampm = hour >= 12 ? "PM" : "AM";
     let dayName = ["SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT"];
 
     hour = hour % 12;
     hour = hour < 10 ? "0" + hour : hour;
     minute = minute < 10 ? "0" + minute : minute; 
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
     document.getElementById("day").innerHTML = dayName[dayNumb];
     document.getElementById("hr").innerHTML = hour;
     document.getElementById("min").innerHTML = minute;
+    document.getElementById("sec").innerHTML = seconds;
     document.getElementById("pm").innerHTML = ampm;
 
     setTimeout(calculateTime, 100);
